1941cd3a2074934a5a227e193c4f6b682f54e26e
nghianinja <ssgarenss@gmail.com>
2024-03-08 13:02:54 +0700

    nghiadeptrai

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Hạnh Siu Xinh';
+const giftUrl = 'https://www.facebook.com/hanhbui0311';
+const eventName = 'Đôi lời ngày 8 tháng 3';
+const titleCard = 'Tặng Hạnh siu xinh';
+const contentCard = 'Chúc người yêu dễ thương và ngoan hiền dịu nhất quả đất .Chúc bạn tất cả những điều tốt đẹp nhất';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
